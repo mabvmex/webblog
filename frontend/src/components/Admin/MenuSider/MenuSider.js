71cedb52c67691dd043c144bde2df85daad13161
mabvmex <mabvmex@gmail.com>
2020-10-31 22:51:23 -0600

    cambio fijo del menu al recargar la pagina

--- frontend/src/components/Admin/MenuSider/MenuSider.js
+++ frontend/src/components/Admin/MenuSider/MenuSider.js
@@ -1,30 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { Layout, Menu } from "antd";
-import { MenuOutlined, HomeOutlined, UserOutlined } from "@ant-design/icons";
+import { HomeOutlined, UserOutlined } from '@ant-design/icons';
 
-import "./MenuSider.scss";
+import './MenuSider.scss';
 
-export default function AdminSider(props) {
-  const { menuCollapsed } = props;
+function MenuSider( props ) {
+  const { menuCollapsed, location } = props;
   const { Sider } = Layout;
+
   return (
-    <Sider className="admin-sider" theme="dark" collapsed={menuCollapsed}>
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-        <Menu.Item key="1">
-          <Link to={"/admin"}>
-            <HomeOutlined />
-            <span className="nav-text"> home </span>
+    <Sider className="admin-sider" theme = 'dark' collapsed = { menuCollapsed }>
+      <Menu theme = "dark" mode = "inline" defaultSelectedKeys = {[location.pathname]}>
+        <Menu.Item key = "/admin">
+          <Link to = {'/admin'}>
+          <HomeOutlined/>
+            <span className = "nav-text"> home </span>
           </Link>
         </Menu.Item>
-        <Menu.Item key="2">
-          <Link to={"/admin/users"}>
-            <UserOutlined>
-              <span className="nav-text">Usuarios</span>
-            </UserOutlined>
-          </Link>
+        <Menu.Item key='/admin/users'>
+            <Link to = {'/admin/users'}>
+            <UserOutlined/>
+                <span className = 'nav-text'> Users </span>
+            </Link>
         </Menu.Item>
       </Menu>
     </Sider>
   );
 }
+
+export default withRouter(MenuSider);
