4454c8940c1b53e97e201e3f2f926708702aa64a
mabvmex <mabvmex@gmail.com>
2020-11-03 10:39:59 -0600

    CLIENTE: Endpoint obtencion usuarios activos e inactivos y recuperarlos en el cliente 2/2

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -34,7 +34,6 @@ export function signUpApi(data) {
     });
 }
 
-
 export function signInApi (data) {
   const url = `${basePath}/${apiVersion}/signin`;
   const params = {
@@ -59,6 +58,28 @@ export function signInApi (data) {
 
 export function getUsersApi(token) {
   const url = `${basePath}/${apiVersion}/users`;
+
+  const params = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token
+    }
+  };
+
+  return fetch(url, params)
+  .then(response => {
+    return response.json()
+  })
+  .then(result => {
+    return result;
+  }).catch(err => {
+    return err.message
+  });
+}
+
+export function getUsersActiveApi(token, status) {
+  const url = `${basePath}/${apiVersion}/users-active?active=${status}`;
 
   const params = {
     method: 'GET',
--- frontend/src/pages/admin/Users/Users.js
+++ frontend/src/pages/admin/Users/Users.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { getAccessTokenApi } from '../../../api/auth';
-import { getUsersApi } from '../../../api/user';
+import { getUsersActiveApi } from '../../../api/user';
 import './Users.scss'
 
 export default function Users() {
-    const [users, setUsers] = useState([]);
+    const [usersActive, setUsersActive] = useState([]);
+    const [usersInactive, setUsersInactive] = useState([]);
     const token = getAccessTokenApi();
 
+    console.log('usersActive: ', usersActive);
+    console.log('usersNOactive: ', usersInactive);
 
-    console.log(users);
     useEffect(() => {
-        getUsersApi(token).then(response => {
-            setUsers(response);
+        getUsersActiveApi(token, true).then(response => {
+            setUsersActive(response);
+        });
+        getUsersActiveApi(token, false).then(response => {
+            setUsersInactive(response);
         });
     }, [token]);
-
+    
     return (
         <>
         <h1> Lista de usuarios</h1>
