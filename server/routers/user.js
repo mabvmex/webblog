5e971f1ddb5706ff14ba1f8093ad442c9b4f9836
mabvmex <mabvmex@gmail.com>
2020-11-09 20:52:35 -0600

    CLIENT: Relocated / EndPoint para recuperar la URL del avatar

---
+++ server/routers/user.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const UserController = require('../controllers/user');
+const multipart = require('connect-multiparty');
+const middleware_auth = require('../middleware/autenticated');
+const middleware_upload_avatar = multipart({ uploadDir: './uploads/avatar' })
+
+const api = express.Router();
+
+api.post('/signup', UserController.signUp);
+api.post('/signin', UserController.signIn);
+api.get('/users', [middleware_auth.ensureAuth], UserController.getUsers);
+api.get('/users-active', [middleware_auth.ensureAuth], UserController.getUsersActive);
+api.put('/upload-avatar/:id', [middleware_auth.ensureAuth, middleware_upload_avatar], UserController.uploadAvatar)
+api.get('/get-avatar/:avatarName', UserController.getAvatar)
+
+module.exports = api;
