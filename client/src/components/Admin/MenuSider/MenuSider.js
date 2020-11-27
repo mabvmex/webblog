0bea4616de8e7b0beba2229512a84c9fafb1e0a4
mabvmex <mabvmex@gmail.com>
2020-11-27 13:10:37 -0600

    CLIENTE: Creando el menu web en el panel del admin

--- client/src/components/Admin/MenuSider/MenuSider.js
+++ client/src/components/Admin/MenuSider/MenuSider.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import { Layout, Menu } from "antd";
-import { HomeFilled, UserOutlined } from '@ant-design/icons';
+import { HomeFilled, UserOutlined, MenuOutlined } from '@ant-design/icons';
 
 import './MenuSider.scss';
 
@@ -22,6 +22,12 @@ function MenuSider( props ) {
             <Link to = {'/admin/users'}>
             <UserOutlined/>
                 <span className = 'nav-text'> Users </span>
+            </Link>
+        </Menu.Item>
+        <Menu.Item key='/admin/menu'>
+            <Link to = {'/admin/menu'}>
+            <MenuOutlined/>
+                <span className = 'nav-text'> Menu </span>
             </Link>
         </Menu.Item>
       </Menu> 
--- client/src/config/routes.js
+++ client/src/config/routes.js
@@ -6,6 +6,7 @@ import LayoutBasic from '../layouts/LayoutBasic';
 import AdminHome from '../pages/admin/Index' // (from index.js)
 import AdminSignIn from '../pages/admin/SignIn';
 import AdminUsers from '../pages/admin/Users';
+import AdminMenuWeb from '../pages/admin/MenuWeb';
 
 // Basic Pages
 import Home from '../pages/Home';
@@ -34,6 +35,11 @@ const routes = [
             {
                 path: '/admin/users',
                 component: AdminUsers,
+                exact: true
+            },
+            {
+                path: '/admin/menu',
+                component: AdminMenuWeb,
                 exact: true
             },
             {
