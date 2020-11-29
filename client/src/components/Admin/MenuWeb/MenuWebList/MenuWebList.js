72e45983d7055a180e1d85bdb49e8586a3437cca
mabvmex <mabvmex@gmail.com>
2020-11-29 00:04:01 -0600

    CLIENTE: Pintando lista de menus tipo Drag&Drop

--- client/src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
+++ client/src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
@@ -1,17 +1,64 @@
 import React, {useState, useEffect} from 'react';
+import { Switch, List, Button, Modal as ModalAntd, notification } from 'antd';
+import { EditFilled, DeleteFilled } from '@ant-design/icons';
+import Modal from '../../../Modal';
+import DragSortableList  from 'react-drag-sortable';
+import './MenuWebList.scss';
+
+
+const { confirm } = ModalAntd;
 
 export default function MenuWebList(props) {
     const { menu, setReloadMenuWeb } = props;
 
-    console.log(menu)
+    const [ listItems, setListItems ] = useState([]);
+    const [ isVisibleModal, setIsVisibleModal ] = useState(false);
+    const [ modalTitle, setModalTitle ] = useState("");
+    const [ modalContent, setModalContent ] = useState(null);
 
+    useEffect( () => {
+        const listItemsArray = [];
+        
+        menu.forEach(element => {
+            listItemsArray.push({
+            content: <MenuItem element={element} />
+            });
+        });
+        
+        setListItems(listItemsArray);
+
+    }, [menu])
+
+
+    const onSort = (sortedList, dropEvent) => {
+        console.log(sortedList)
+    }
+    
     return (
-        <div>
-            <h1> == MENUWEBLIST == </h1>
-            {menu.map (item => (
-            <p key={item._id}> {item.title} </p>
-            ))}
+        <div className='menu-web-list'>
+            <div className='menu-web-list__header'>
+                <Button type='primary' shape='round' > Menú web </Button>
+            </div>
+
+            <div className='menu-web-list__items'>
+                <DragSortableList items={listItems} onSort={onSort} type='vertical' />
+            </div>
 
         </div>
+    )
+}
+
+function MenuItem(props) {
+    const {element} = props;
+
+    return (
+        <List.Item
+            actions={[
+                <Switch defaultChecked = {element.active} />,
+                <Button type='primary' shape='circle'> <EditFilled />  </Button>,
+                <Button type='danger' shape='circle'> <EditFilled />  </Button>
+            ]}>
+            <List.Item.Meta title={element.title} description={element.url} />
+        </List.Item>
     )
 }
