3507910d8c61c01e79ef105b92cdf631dd92776b
mabvmex <mabvmex@gmail.com>
2020-11-29 12:30:00 -0600

    Actualizar/guardar el orden de un menu en la DB desde el backOffice

--- client/src/api/menu.js
+++ client/src/api/menu.js
@@ -1,6 +1,6 @@
 import { basePath, apiVersion } from './config';
 
-export function getMenuApi () {
+export function getMenuApi() {
     const url = `${basePath}/${apiVersion}/get-menu`;
 
     return (
@@ -15,4 +15,28 @@ export function getMenuApi () {
                return  err.message;
             })
     )
+}
+
+ export function updateMenuApi(token, menuId, data) {
+    const url = `${basePath}/${apiVersion}/update-menu/${menuId}`;
+
+    const params = {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': token
+        },
+        body: JSON.stringify(data)
+    };
+
+    return fetch(url, params)
+    .then (response => {
+        return response.json();
+    })
+    .then (result => {
+        return result.message;
+    })
+    .catch(err => {
+       return  err;
+    });
 }
--- client/src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
+++ client/src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
@@ -3,6 +3,8 @@ import { Switch, List, Button, Modal as ModalAntd, notification } from 'antd';
 import { EditFilled, DeleteFilled } from '@ant-design/icons';
 import Modal from '../../../Modal';
 import DragSortableList  from 'react-drag-sortable';
+import { updateMenuApi } from '../../../../api/menu';
+import { getAccessTokenApi } from '../../../../api/auth'
 import './MenuWebList.scss';
 
 
@@ -31,7 +33,14 @@ export default function MenuWebList(props) {
 
 
     const onSort = (sortedList, dropEvent) => {
-        console.log(sortedList)
+        const accessToken = getAccessTokenApi();
+
+        sortedList.forEach(element => {
+            const { _id } = element.content.props.element;
+            const order = element.rank;
+
+            updateMenuApi(accessToken, _id, {order});
+        })
     }
     
     return (
@@ -55,8 +64,8 @@ function MenuItem(props) {
         <List.Item
             actions={[
                 <Switch defaultChecked = {element.active} />,
-                <Button type='primary' shape='circle'> <EditFilled />  </Button>,
-                <Button type='danger' shape='circle'> <EditFilled />  </Button>
+                <Button type='primary' shape='circle' size='large'> <EditFilled />  </Button>,
+                <Button type='danger' shape='circle' size='large'> <DeleteFilled />  </Button>
             ]}>
             <List.Item.Meta title={element.title} description={element.url} />
         </List.Item>
