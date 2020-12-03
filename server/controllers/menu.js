4cbe61b4990825afb0937db4db719b9e0a29040c
mabvmex <mabvmex@gmail.com>
2020-12-02 20:08:23 -0600

    SERVER: EndPoint para eliminar menus

--- server/controllers/menu.js
+++ server/controllers/menu.js
@@ -102,10 +102,32 @@ function activateMenu(req, res) {
     })
 }
 
+function deleteMenu(req, res) {
+    const { id } = req.params;
+
+    Menu.findByIdAndRemove(id, (err, menuDeleted) => {
+        if(err) {
+            res.status(500).send({
+                message: 'Error del servidor'
+            });
+        } else {
+            if(!menuDeleted) {
+                res.status(404).send({
+                    message: 'Menú no encontrado'
+                });
+            } else {
+                res.status(200).send({
+                    message: 'El menú ha sido eliminado exitosamente'
+                });
+            }
+        }
+    });
+}
 
 module.exports = {
   addMenu,
   getMenu,
   updateMenu,
-  activateMenu
+  activateMenu,
+  deleteMenu,
 };
--- server/routers/menu.js
+++ server/routers/menu.js
@@ -8,4 +8,6 @@ api.post('/add-menu', [middleware_auth.ensureAuth], MenuController.addMenu);
 api.get('/get-menu', MenuController.getMenu);
 api.put('/update-menu/:id', [middleware_auth.ensureAuth], MenuController.updateMenu);
 api.put('/activate-menu/:id', [middleware_auth.ensureAuth], MenuController.activateMenu);
+api.delete('/delete-menu/:id', [middleware_auth.ensureAuth], MenuController.deleteMenu);
+
 module.exports = api;
