31d2ad903ed97b14169f705f1789ea7d2eb27c93
mabvmex <mabvmex@gmail.com>
2021-01-03 15:39:23 -0600

    Server: EndPoint para eliminar cursos

--- server/controllers/course.js
+++ server/controllers/course.js
@@ -52,7 +52,35 @@ function getCourses(req, res) {
     });
 }
 
+function deleteCourse(req, res) {
+  const { id } = req.params;
+
+  Course.findByIdAndRemove(id, (err, courseDeleted) => {
+    if (err) {
+      res.status(500).send({
+        code: 500,
+        message: "Error del servidor",
+      });
+    } else {
+      if (!courseDeleted) {
+        res.status(404).send({
+          code: 404,
+          message: "Curso no encontrado",
+        });
+      } else {
+        res.status(200).send({
+          code: 200,
+          message: "Curso eliminado correctamente",
+        });
+      }
+    }
+  });
+
+  console.log(id);
+}
+
 module.exports = {
   addCourse,
   getCourses,
+  deleteCourse,
 };
--- server/routers/courses.js
+++ server/routers/courses.js
@@ -6,5 +6,6 @@ const api = express.Router();
 
 api.post('/add-course', [middleware_auth.ensureAuth], CourseController.addCourse);
 api.get('/get-courses', CourseController.getCourses);
+api.delete('/delete-course/:id', [middleware_auth.ensureAuth], CourseController.deleteCourse);
 
 module.exports = api;
