9162de5230b8533526750402966d89e4246e076a
mabvmex <mabvmex@gmail.com>
2021-01-05 23:52:18 -0600

    SERVER: Obteniendo datos de cursos API Udemy

--- client/src/api/course.js
+++ client/src/api/course.js
@@ -15,3 +15,20 @@ export function getCoursesApi() {
     })
 
 }
+
+export function getCoursesDataUdemyApi( id ) {
+    const baseUrl = `https://www.udemy.com/api-2.0/courses/${id}`
+    const coursesParams = `?fields[course]=title,headline,url,price,image_480x280`
+    const url = baseUrl + coursesParams;
+
+    return fetch(url)
+    .then(async response => {
+        return { code: response.status, data: await response.json() };
+    })
+    .then(result => {
+        return result;
+    })
+    .catch(err => {
+        return err;
+    });
+}
