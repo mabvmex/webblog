ba48eda285968a28bcdf00a5e4cd928be6f3dbdd
mabvmex <mabvmex@gmail.com>
2021-01-14 00:04:37 -0600

    CLIENTE: creacion de PresentationCourse

--- client/src/components/Web/Courses/PresentationCourses/PresentationCourses.js
+++ client/src/components/Web/Courses/PresentationCourses/PresentationCourses.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import './PresentationCourses.scss';
+import React from "react";
+import SchoolLogo from "../../../../assets/img/png/academy-logo.png";
+import "./PresentationCourses.scss";
 
 export default function PresentationCourses() {
-    return (
-        <div>
-            <h2>=== PRESENTATION-COURSES ===</h2>
-        </div>
-    )
+  return (
+    <div className="presentation-courses">
+      <img src={SchoolLogo} alt="Cursos de udemy" />
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua."
+      </p>
+      <p>
+        "Res publica non dominetur"
+      </p>
+    </div>
+  );
 }
