2ea4b9b5effe5dbcfeaf049dbf5135bc4ba83bc2
mabvmex <mabvmex@gmail.com>
2021-01-16 23:56:53 -0600

    CLIENTE: obteniendo la info publica de cada elemento de la API de UDEMY

--- client/src/components/Web/Courses/CoursesList/CoursesList.js
+++ client/src/components/Web/Courses/CoursesList/CoursesList.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import './CoursesList.scss';
+import React from "react";
+import { Row, Col, Card, Button, Rate, notification } from "antd";
+import { getCoursesDataUdemyApi } from "../../../../api/course";
+import "./CoursesList.scss";
 
 export default function CoursesList(props) {
-    const { courses } = props;
+  const { courses } = props;
 
-    console.log(courses);
-
-    return (
-        <div>
-            <h2> === COURSES-LIST === </h2>
-        </div>
-    )
+  return (
+      <div className='courses-list' >
+          <Row>
+              <h1>hola</h1>
+          </Row>
+      </div>
+  )
 }
+
+function MostrarCourse(props) {
+  const { course } = props;
+  console.log(course);
+
+  return <p> === HOLA ===</p>;
+}
+
+
+// { courses.map(course => (
+//     <Col md={8} className='courses-list_course'>
+//         <MostrarCourse key={course._id}> course={course} </MostrarCourse>
+//     </Col>
+  
+// )) }
