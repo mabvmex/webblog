cc69424cea85af696e03a38d7450e3e4753a801d
mabvmex <mabvmex@gmail.com>
2021-01-17 17:52:21 -0600

    CLIENTE: Dibujando todos los cursos por pantalla + url de cupon

--- client/src/components/Web/Courses/CoursesList/CoursesList.js
+++ client/src/components/Web/Courses/CoursesList/CoursesList.js
@@ -4,44 +4,77 @@ import { getCoursesDataUdemyApi } from "../../../../api/course";
 import "./CoursesList.scss";
 
 export default function CoursesList(props) {
-  const {courses} = props;
-
-  console.log(courses)
+  const { courses } = props;
 
   return (
-      <div className='courses-list' >
-          <Row>
-              {courses.map(course => (
-                <Col md={8} className='courses-list__course' key={course._id} >
-                    <Course  course={course} />
-                </Col>
-               )) }
-          </Row>
-      </div>
-  )
+    <div className="courses-list">
+      <Row>
+        {courses.map((course) => (
+          <Col md={8} className="courses-list__course" key={course._id}>
+            <Course course={course} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 }
 
 function Course(props) {
   const { course } = props;
   const [courseInfo, setCourseInfo] = useState({});
+  const [urlCourse, setUrlCourse] = useState("");
+  const { Meta } = Card;
 
   useEffect(() => {
-      getCoursesDataUdemyApi(course.idCourse)
-        .then(response => {
-            if(response?.code !== 200){
-                notification['warning']({
-                    message: response.message
-                });
-            } else {
-                setCourseInfo(response.data)
-            }
-        })
-        .catch(err => {
-            notification['error']({
-                message: 'Error del servidor, inténtelo más tarde'
-            });
-        })
-  }, [course])
+    getCoursesDataUdemyApi(course.idCourse)
+      .then((response) => {
+        if (response?.code !== 200) {
+          notification["warning"]({
+            message: response.message,
+          });
+        } else {
+          setCourseInfo(response.data);
+          mountUrl(response.data.url);
+        }
+      })
+      .catch((err) => {
+        notification["error"]({
+          message: "Error del servidor, inténtelo más tarde",
+        });
+      });
+  }, [course]);
 
-  return <p> {courseInfo.title} </p>;
+  const mountUrl = (url) => {
+    if (!course.link) {
+      const baseUrl = `https://www.udemy.com${url}`;
+      const finalUrl =
+        baseUrl + (course.coupon ? `?couponCode=${course.coupon}` : "");
+      setUrlCourse(finalUrl);
+    } else {
+      setUrlCourse(course.link);
+    }
+  };
+
+  return (
+    <a href={urlCourse} target="_blank" rel="noopener noreferrer">
+      <Card
+        cover={<img src={courseInfo.image_100x100} alt={courseInfo.title} />}
+      >
+        <Meta title={courseInfo.title} description={courseInfo.headline} />
+
+        <Button> Ver curso </Button>
+        <div className="courses-list__course-footer">
+          
+          <span className="courses-list__course-price">
+            
+            {course.price ? `MXN$${course.price}` : courseInfo.price}
+          </span>
+        </div>
+        <div>
+          
+          <Rate disabled defaultValue={5} />
+        </div>
+      </Card>
+    </a>
+  );
 }
