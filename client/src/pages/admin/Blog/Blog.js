2a32747a8703a23865d9618f616f57218b62a409
mabvmex <mabvmex@gmail.com>
2021-02-04 14:44:32 -0600

    CLIENTE: Estructura basica de la seccion del blog

--- client/src/pages/admin/Blog/Blog.js
+++ client/src/pages/admin/Blog/Blog.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import './Blog.scss';
+import React, { useState, useEffect } from "react";
+import { Button, notification } from "antd";
+import Modal from "../../../components/Modal";
+import "./Blog.scss";
 
 export default function Blog() {
-    return (
-        <div>
-            <h1> === BLOG === </h1>
-        </div>
-    )
+  const [isVisibleModal, setIsVisibleModal] = useState(false);
+  const [modalTitle, serModalTitle] = useState("");
+  const [modalContent, setModalContent] = useState(null);
+
+  return (
+    <div className="blog">
+      <div className="blog__add-post">
+        <Button type="primary">Nuevo post</Button>
+      </div>
+
+      <h1>=== POSTS_LIST ===</h1>
+      <h2> === Paginación ===</h2>
+
+      <Modal
+        title={modalTitle}
+        isVisible={isVisibleModal}
+        setIsVisible={setIsVisibleModal}
+        width="75%"
+      />
+    </div>
+  );
 }
