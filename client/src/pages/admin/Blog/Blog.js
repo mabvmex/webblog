591f3b908cabafd4492293d8e682188403b3d21d
mabvmex <mabvmex@gmail.com>
2021-02-05 23:52:00 -0600

    CLIENTE: obteniendo los posts de la DB con paginacion

--- client/src/pages/admin/Blog/Blog.js
+++ client/src/pages/admin/Blog/Blog.js
@@ -1,12 +1,40 @@
 import React, { useState, useEffect } from "react";
 import { Button, notification } from "antd";
+import { withRouter } from "react-router-dom";
+import queryString from "query-string";
 import Modal from "../../../components/Modal";
 import "./Blog.scss";
+import { getPostsApi } from "../../../api/post";
 
-export default function Blog() {
+function Blog(props) {
+  const { location, history } = props;
+  const [posts, setPosts] = useState(null)
+  const [reloadPosts, setReloadPosts] = useState(false)
   const [isVisibleModal, setIsVisibleModal] = useState(false);
   const [modalTitle, serModalTitle] = useState("");
   const [modalContent, setModalContent] = useState(null);
+  const { page = 1 } = queryString.parse(location.search);
+
+console.log(posts);
+
+  useEffect(() => {
+      getPostsApi(20, page)
+      .then(response => {
+          if(response?.code !==200) {
+              notification['warning']({
+                  message: response.message
+              });
+          } else {
+              setPosts(response.posts);
+          }
+      })
+      .catch(()=>{
+        notification['error']({
+            message: 'Error del servidor'
+        });
+      });
+      setReloadPosts(false)
+  }, [page, reloadPosts]);
 
   return (
     <div className="blog">
@@ -26,3 +54,5 @@ export default function Blog() {
     </div>
   );
 }
+
+export default withRouter(Blog);
